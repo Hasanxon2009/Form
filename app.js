67d704c0bc8282003236c76b6aaba8eff559a553
Hasanxon2009 <jorayevhasanxon477@gmail.com>
2024-02-13 20:13:08 +0500

    linear-gradient

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
-const form = document.querySelector("#form-body");
-const formText = document.querySelector("#form-text");
-const message = document.querySelector(".message");
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const text = document.querySelector(".text");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const input = /^[a-zA-Z0-9]{6,12}$/;
-  const value = formText.value;
-  if (input.test(value)) {
-    message.textContent = "Ma'lumot to'g'ri";
-  } else {
-    message.textContent =
-      "Kiritilgan ma'lumot 6 va 12 oralig'ida katta-kichik va raqamlardan iborat bo`lishi kerak";
+const value = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+function getGradient() {
+  let age = "#";
+  for (i = 0; i < 6; i++) {
+    const renderNumber = Math.floor(Math.random() * value.length);
+    age += value[renderNumber];
   }
-  form.reset();
-});
+  return age;
+}
+
+function setGradient() {
+  const redmi = getGradient();
+  const xiomi = getGradient();
+  const deg = Math.floor(Math.random() * 360);
+  const color = `linear-gradient(
+    ${deg}deg,
+    ${redmi},
+    ${xiomi}
+  )`;
+  body.style.background = color;
+  text.textContent = color;
+}
+setGradient();
+container.addEventListener("click", setGradient);
